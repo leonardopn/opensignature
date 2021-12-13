da89ddcae92f3a285c951327e5354f46f1fe2ca8
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-12-13 14:41:47 -0300

    feat(Component: Canvas): :sparkles: Retirando funções de zoom e rotação e deixando mais simplista

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -3,14 +3,11 @@ import React, { Dispatch, SetStateAction } from "react";
 
 interface canvasProps {
     setPosition: Dispatch<SetStateAction<{ x: number; y: number }>>;
-    rotate: number;
     image: HTMLImageElement | undefined;
     sizes: { width: number; height: number };
 }
 
-export const Canvas = ({ setPosition, rotate, image, sizes }: canvasProps) => {
-    const [positionAbsolute, setPositionAbsolute] = React.useState({ top: "30px" });
-
+export const Canvas = ({ setPosition, image, sizes }: canvasProps) => {
     function setCoordinates(x: number, y: number) {
         setPosition({
             x,
@@ -18,32 +15,22 @@ export const Canvas = ({ setPosition, rotate, image, sizes }: canvasProps) => {
         });
     }
 
-    React.useEffect(() => {
-        switch (rotate) {
-            case 0:
-                setPositionAbsolute({ top: "30px" });
-                break;
-            case 180:
-                setPositionAbsolute({ top: "30px" });
-                break;
-            default:
-                setPositionAbsolute({ top: "400px" });
-                break;
-        }
-    }, [rotate]);
-
     return (
-        <Box bg="gray" padding="8" position="relative" w={sizes.width + 1000} h={sizes.height + 1000}>
+        <Box
+            p="8"
+            bg="#e9e9e9"
+            minWidth={sizes.width + 80}
+            minHeight="100%"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+        >
             <Image
-                position="absolute"
                 onMouseMove={(e) => {
                     const bound = e.currentTarget.getBoundingClientRect();
                     setCoordinates(e.clientX - bound.left, e.clientY - bound.top);
                 }}
-                style={{
-                    transform: `rotate(${rotate}deg)`,
-                }}
-                src={"/teste2.png"}
+                src={"/teste.png"}
                 width={sizes.width}
                 height={sizes.height}
                 maxWidth="none"
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,6 @@
-import { Box, Button, ChakraProvider, Flex, HStack, Text } from "@chakra-ui/react";
+import { Box, ChakraProvider, Flex, HStack, Select, Text } from "@chakra-ui/react";
 import React from "react";
-import { FiRotateCw } from "react-icons/fi";
-import { RiZoomInLine, RiZoomOutLine } from "react-icons/ri";
+import { GoArrowDown } from "react-icons/go";
 import useImage from "use-image";
 import { Canvas } from "../components/Canvas";
 import { SelectorBar } from "../components/SelectorBar";
@@ -9,39 +8,15 @@ import { theme } from "../styles/theme/theme";
 
 export const App = () => {
     const [coordinates, setCoordinates] = React.useState({ x: 0, y: 0 });
-    const [rotate, setRotate] = React.useState(0);
     const [sizes, setSizes] = React.useState({ width: 0, height: 0 });
     const [zoom, setZoom] = React.useState(1);
-    const [imagePdf] = useImage("/teste2.png");
+    const [imagePdf] = useImage("/teste.png");
 
     React.useEffect(() => {
         if (imagePdf?.width && imagePdf?.height) {
-            setSizes({ width: imagePdf?.width, height: imagePdf?.height });
+            setSizes({ width: imagePdf?.width * zoom, height: imagePdf?.height * zoom });
         }
-    }, [imagePdf]);
-
-    function rotateRigth() {
-        if (rotate + 90 < 360) {
-            setRotate(rotate + 90);
-        } else {
-            setRotate(0);
-        }
-    }
-
-    function zoomIn() {
-        if (imagePdf?.width && imagePdf?.height) {
-            console.log(imagePdf?.width + "*" + (zoom + 0.1));
-            setSizes({ width: imagePdf?.width * (zoom + 0.1), height: imagePdf?.height * (zoom + 0.1) });
-            setZoom(zoom + 0.1);
-        }
-    }
-    function zoomOut() {
-        if (imagePdf?.width && imagePdf?.height) {
-            console.log(imagePdf?.width + "*" + (zoom - 0.1));
-            setSizes({ width: imagePdf?.width * (zoom - 0.1), height: imagePdf?.height * (zoom - 0.1) });
-            setZoom(zoom - 0.1);
-        }
-    }
+    }, [zoom, imagePdf]);
 
     return (
         <ChakraProvider theme={theme}>
@@ -49,21 +24,37 @@ export const App = () => {
                 <SelectorBar></SelectorBar>
                 <Flex m="8" flex="1" direction="column">
                     <HStack spacing="2" mb="3">
-                        <Button colorScheme="green" leftIcon={<RiZoomInLine />} onClick={zoomIn}>
-                            Zoom In
-                        </Button>
-                        <Button colorScheme="red" leftIcon={<RiZoomOutLine />} onClick={zoomOut}>
-                            Zoom Out
-                        </Button>
-                        <Button colorScheme="blue" leftIcon={<FiRotateCw />} onClick={rotateRigth}>
-                            Rodar
-                        </Button>
+                        <Select
+                            bg="pink.500"
+                            fontWeight="bold"
+                            borderColor="pink.500"
+                            cursor="pointer"
+                            _active={{ color: "black" }}
+                            icon={<GoArrowDown></GoArrowDown>}
+                            value={zoom}
+                            onChange={(e) => setZoom(+e.target.value)}
+                            width="fit-content"
+                        >
+                            <option value="0.1">10%</option>
+                            <option value="0.2">20%</option>
+                            <option value="0.3">30%</option>
+                            <option value="0.4">40%</option>
+                            <option value="0.5">50%</option>
+                            <option value="0.5">60%</option>
+                            <option value="0.7">70%</option>
+                            <option value="0.8">80%</option>
+                            <option value="0.9">90%</option>
+                            <option value="1">100%</option>
+                            <option value="1.1">110%</option>
+                            <option value="1.2">120%</option>
+                        </Select>
                         <Text>
-                            X: {coordinates.x}, Y: {coordinates.y}
+                            <strong>X:</strong> {Math.round(coordinates.x)}, <strong>Y:</strong>{" "}
+                            {Math.round(coordinates.y)}
                         </Text>
                     </HStack>
-                    <Box flex="1" overflow="auto" maxWidth={window.innerWidth - 220}>
-                        <Canvas sizes={sizes} image={imagePdf} setPosition={setCoordinates} rotate={rotate}></Canvas>
+                    <Box id="scroll-box" flex="1" overflow="auto" maxWidth={window.innerWidth - 220}>
+                        <Canvas sizes={sizes} image={imagePdf} setPosition={setCoordinates}></Canvas>
                     </Box>
                 </Flex>
             </Flex>
