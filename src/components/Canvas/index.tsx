99cfac940a9e613ae68aa23e4322f12d80322b1c
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-12-13 09:28:59 -0300

    feat(Component: Canvas): :poop: Tentando implementar rotação de zoom, porém nada funcional

--- src/components/Canvas/index.tsx
+++ src/components/Canvas/index.tsx
@@ -1,14 +1,15 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
 import React, { Dispatch, SetStateAction } from "react";
-import useImage from "use-image";
 
 interface canvasProps {
     setPosition: Dispatch<SetStateAction<{ x: number; y: number }>>;
+    rotate: number;
+    image: HTMLImageElement | undefined;
+    sizes: { width: number; height: number };
 }
 
-export const Canvas = ({ setPosition }: canvasProps) => {
-    const [imageButton] = useImage("/assets/button_sign.png");
-    const [imagePdf] = useImage("/teste2.png");
+export const Canvas = ({ setPosition, rotate, image, sizes }: canvasProps) => {
+    const [positionAbsolute, setPositionAbsolute] = React.useState({ top: "30px" });
 
     function setCoordinates(x: number, y: number) {
         setPosition({
@@ -17,15 +18,36 @@ export const Canvas = ({ setPosition }: canvasProps) => {
         });
     }
 
+    React.useEffect(() => {
+        switch (rotate) {
+            case 0:
+                setPositionAbsolute({ top: "30px" });
+                break;
+            case 180:
+                setPositionAbsolute({ top: "30px" });
+                break;
+            default:
+                setPositionAbsolute({ top: "400px" });
+                break;
+        }
+    }, [rotate]);
+
     return (
-        <Box
-            backgroundImage={"/teste2.png"}
-            width={imagePdf?.width}
-            height={imagePdf?.height}
-            onMouseMove={(e) => {
-                const bound = e.currentTarget.getBoundingClientRect();
-                setCoordinates(e.clientX - bound.left, e.clientY - bound.top);
-            }}
-        ></Box>
+        <Box bg="gray" padding="8" position="relative" w={sizes.width + 1000} h={sizes.height + 1000}>
+            <Image
+                position="absolute"
+                onMouseMove={(e) => {
+                    const bound = e.currentTarget.getBoundingClientRect();
+                    setCoordinates(e.clientX - bound.left, e.clientY - bound.top);
+                }}
+                style={{
+                    transform: `rotate(${rotate}deg)`,
+                }}
+                src={"/teste2.png"}
+                width={sizes.width}
+                height={sizes.height}
+                maxWidth="none"
+            ></Image>
+        </Box>
     );
 };
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,37 +1,71 @@
-import { Box, ChakraProvider, Flex, HStack, Button, Text } from "@chakra-ui/react";
+import { Box, Button, ChakraProvider, Flex, HStack, Text } from "@chakra-ui/react";
+import React from "react";
 import { FiRotateCw } from "react-icons/fi";
 import { RiZoomInLine, RiZoomOutLine } from "react-icons/ri";
+import useImage from "use-image";
 import { Canvas } from "../components/Canvas";
 import { SelectorBar } from "../components/SelectorBar";
 import { theme } from "../styles/theme/theme";
-import React from "react";
 
 export const App = () => {
     const [coordinates, setCoordinates] = React.useState({ x: 0, y: 0 });
+    const [rotate, setRotate] = React.useState(0);
+    const [sizes, setSizes] = React.useState({ width: 0, height: 0 });
+    const [zoom, setZoom] = React.useState(1);
+    const [imagePdf] = useImage("/teste2.png");
+
+    React.useEffect(() => {
+        if (imagePdf?.width && imagePdf?.height) {
+            setSizes({ width: imagePdf?.width, height: imagePdf?.height });
+        }
+    }, [imagePdf]);
+
+    function rotateRigth() {
+        if (rotate + 90 < 360) {
+            setRotate(rotate + 90);
+        } else {
+            setRotate(0);
+        }
+    }
+
+    function zoomIn() {
+        if (imagePdf?.width && imagePdf?.height) {
+            console.log(imagePdf?.width + "*" + (zoom + 0.1));
+            setSizes({ width: imagePdf?.width * (zoom + 0.1), height: imagePdf?.height * (zoom + 0.1) });
+            setZoom(zoom + 0.1);
+        }
+    }
+    function zoomOut() {
+        if (imagePdf?.width && imagePdf?.height) {
+            console.log(imagePdf?.width + "*" + (zoom - 0.1));
+            setSizes({ width: imagePdf?.width * (zoom - 0.1), height: imagePdf?.height * (zoom - 0.1) });
+            setZoom(zoom - 0.1);
+        }
+    }
 
     return (
         <ChakraProvider theme={theme}>
-            <Flex>
+            <Flex height="100vh">
                 <SelectorBar></SelectorBar>
-                <Box m="8" flex="1">
+                <Flex m="8" flex="1" direction="column">
                     <HStack spacing="2" mb="3">
-                        <Button colorScheme="green" leftIcon={<RiZoomInLine />}>
+                        <Button colorScheme="green" leftIcon={<RiZoomInLine />} onClick={zoomIn}>
                             Zoom In
                         </Button>
-                        <Button colorScheme="red" leftIcon={<RiZoomOutLine />}>
+                        <Button colorScheme="red" leftIcon={<RiZoomOutLine />} onClick={zoomOut}>
                             Zoom Out
                         </Button>
-                        <Button colorScheme="blue" leftIcon={<FiRotateCw />}>
+                        <Button colorScheme="blue" leftIcon={<FiRotateCw />} onClick={rotateRigth}>
                             Rodar
                         </Button>
                         <Text>
                             X: {coordinates.x}, Y: {coordinates.y}
                         </Text>
                     </HStack>
-                    <Box overflow="auto" maxWidth={window.innerWidth - 220} maxHeight={window.innerHeight - 120}>
-                        <Canvas setPosition={setCoordinates}></Canvas>
+                    <Box flex="1" overflow="auto" maxWidth={window.innerWidth - 220}>
+                        <Canvas sizes={sizes} image={imagePdf} setPosition={setCoordinates} rotate={rotate}></Canvas>
                     </Box>
-                </Box>
+                </Flex>
             </Flex>
         </ChakraProvider>
     );
