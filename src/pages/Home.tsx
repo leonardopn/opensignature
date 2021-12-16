c48d48fa5f9a41af4825054097690854eddd4b0f
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-12-16 18:54:50 -0300

    feat(Page: Home): :poop: Inciio da implementação da Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,24 @@
+import { Button, Flex, Heading, VStack, Input } from "@chakra-ui/react";
 import React from "react";
 
 export const Home = () => {
-    return <div>Olá mundo</div>;
+    return (
+        <Flex height="100vh" flexDirection="column">
+            <VStack
+                display="flex"
+                alignItems="none"
+                spacing="4"
+                bg="gray.700"
+                flexDirection="column"
+                m="auto"
+                borderRadius="8"
+                p="8">
+                <Heading alignSelf="center" pb="5" fontSize="25">
+                    Selecionar arquivo PDF
+                </Heading>
+                <Input type="file"></Input>
+                <Button colorScheme="pink">Processar</Button>
+            </VStack>
+        </Flex>
+    );
 };
